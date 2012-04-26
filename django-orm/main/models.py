daa0755a0574fbf230c9e30b6140c7517face7e4
guoqiao <guoqiao@gmail.com>
2012-04-26 22:02:52 +0800

    add django-orm

---
+++ django-orm/main/models.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from django.db import models
+
+class State(models.Model):
+    name = models.CharField(default="",max_length=63)
+    period = models.IntegerField(default=7)
+    total = models.IntegerField(default=1)
+    count = models.IntegerField(default=0)
+    tmplate = models.CharField(default="",max_length=63)
+
+class Customer(models.Model):
+    name = models.CharField(default="",max_length=63)
+    site = models.URLField(default="",max_length=63)
+    addr = models.CharField(default="",max_length=63)
+    country = models.CharField(default="",max_length=63)
+    phone = models.CharField(default="",max_length=63)
+    
+    contact = models.CharField(default="",max_length=63)
+    mobile = models.CharField(default="",max_length=63)
+    email = models.EmailField(default="",max_length=63)
+    
+    create_on = models.DateField()
+    update_on = models.DateField()
+    
+    note = models.TextField(default="",max_length=2047)
