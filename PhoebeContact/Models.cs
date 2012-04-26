eada733f10e3de56fcfb9afb994020a8642479eb
guoqiao <guoqiao@gmail.com>
2012-04-26 22:31:40 +0800

    define model

--- PhoebeContact/Models.cs
+++ PhoebeContact/Models.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 
-public class article
+public class State
 {
-    public long article_id { get; set; }
-    public string title { get; set; }
-    public DateTime date_created { get; set; }
-    public bool draft { get; set; }
-    public string content { get; set; }
+    public int id { get; set; }
+
+    public string name { get; set; }
+    public int    period { get; set; }
+    public int    total { get; set; }
+    public int    count { get; set; }
+    public string template { get; set; }
+}
+
+public class Customer
+{
+    public int id { get; set; }
+
+    public string name { get; set; }
+    public string site { get; set; }
+    public string addr { get; set; }
+    public string country { get; set; }
+    public string phone { get; set; }
+
+    public string contact { get; set; }
+    public string mobile { get; set; }
+    public string email { get; set; }
+
+    public DateTime create_on { get; set; }
+    public DateTime update_on { get; set; }
+
+    public string note { get; set; }
+
+    public int state_id { get; set; }
 }
