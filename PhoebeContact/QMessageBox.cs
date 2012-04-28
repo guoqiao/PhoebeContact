a17e314432b2bad9bcc312e63d32f93e75916134
guoqiao <guoqiao@gmail.com>
2012-04-28 20:35:39 +0800

    rename

--- PhoebeContact/QMessageBox.cs
+++ PhoebeContact/QMessageBox.cs
@@ -4,26 +4,26 @@ using System.Text;
 using System.Windows.Forms;
 namespace PhoebeContact
 {
-    public class QMessageBox
+    public class Popup
     {
         private static readonly String m_caption = "PhoebeContact";
 
-        public static void ShowInfomation(String message)
+        public static void Info(String message)
         {
             MessageBox.Show(message, m_caption + " 信息", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
-        public static void ShowWarning(String message)
+        public static void Warn(String message)
         {
             MessageBox.Show(message, m_caption + " 警告", MessageBoxButtons.OK, MessageBoxIcon.Warning);
         }
 
-        public static void ShowError(String message)
+        public static void Error(String message)
         {
             MessageBox.Show(message, m_caption + " 错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
 
-        public static DialogResult ShowQuestion(String message)
+        public static DialogResult Question(String message)
         {
             return MessageBox.Show(message, m_caption + " 确认", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
         }
