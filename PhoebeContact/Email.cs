674b61c1c0f1cd9a182b7c11755726bc8ab61d32
guoqiao <guoqiao@gmail.com>
2012-04-29 22:25:01 +0800

    parse email config form config.xml

--- PhoebeContact/Email.cs
+++ PhoebeContact/Email.cs
@@ -3,26 +3,37 @@ using System.Collections.Generic;
 using System.Text;
 using System.Net.Mail;
 using System.Net;
+using System.Xml;
 
 namespace PhoebeContact
 {
     class Email
     {
-        private SmtpClient m_client = new SmtpClient("smtp.gmail.com", 587);
-        string m_username = "name@gmail.com";
-        string m_password = "pass";
+        private SmtpClient m_client = new SmtpClient();
+        string m_account = "";
 
         public Email()
         {
             m_client.DeliveryMethod = SmtpDeliveryMethod.Network;
-            m_client.EnableSsl = true;
-            m_client.Credentials = new NetworkCredential(m_username, m_password);
+
+            XmlCare xc = new XmlCare("config.xml");
+            XmlElement xe = xc.GetElement("mail");
+            string smtp = xe.GetAttribute("smtp");
+            int port = int.Parse(xe.GetAttribute("port"));
+            bool ssl = xe.GetAttribute("ssl") == "true"; 
+            string account = xe.GetAttribute("account");
+            string password = xe.GetAttribute("password");
+
+            m_client.Host = smtp;
+            m_client.Port = port;
+            m_client.EnableSsl = ssl;
+            m_client.Credentials = new NetworkCredential(account, password);
+            m_account = account;
         }
 
-        public void SendMail(string to, string subject, string body)
+        public void SendMail(string receiver, string subject, string body)
         {
-            MailMessage mail = new MailMessage(m_username, to, subject, body);
-            mail.IsBodyHtml = true;
+            MailMessage mail = new MailMessage(m_account, receiver, subject, body);
             mail.SubjectEncoding = Encoding.GetEncoding("GB2312");
             mail.BodyEncoding = Encoding.GetEncoding("GB2312");
             m_client.Send(mail);
---
+++ PhoebeContact/XmlCare.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using System.Diagnostics;
+using System.IO;
+
+namespace PhoebeContact
+{
+    class XmlCare
+    {
+        protected XmlDocument m_doc = new XmlDocument();
+        protected XmlElement m_root = null;
+        private string m_file = "";
+
+        public XmlCare(string file)
+        {
+            m_file = file;
+            LoadConfig();
+        }
+
+        public virtual void LoadConfig()
+        {
+            m_doc.Load(m_file);
+            m_root = m_doc.DocumentElement;
+        }
+
+        public XmlElement GetElement(string elem_name)
+        {
+            XmlNodeList nodes = m_root.GetElementsByTagName(elem_name);
+            Debug.Assert(nodes.Count > 0);
+            return nodes[0] as XmlElement;
+        }
+
+        public string GetAttribute(string elem_name, string attr_name)
+        {
+            XmlElement node = GetElement(elem_name);
+            string attr = node.GetAttribute(attr_name);
+            return attr;
+        }
+
+        public void SetAttribute(string elem_name, string attr_name, string attr_value)
+        {
+            XmlElement node = GetElement(elem_name);
+            node.SetAttribute(attr_name, attr_value);
+            Save();
+        }
+
+        public void Save()
+        {
+            m_doc.Save(m_file);
+        }
+    }
+}
